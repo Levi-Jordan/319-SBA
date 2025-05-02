f1a70387e65164c5fd822199084ed183df7f9756
Levi-Jordan <Levijordananderson@gmail.com>
2025-05-02 01:10:48 -0600

    Almost done

--- models/cardioSchema.mjs
+++ models/cardioSchema.mjs
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const cardioSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        index: true
     },
     timeElapsed: { 
         type: Number,
--- models/weightsSchema.mjs
+++ models/weightsSchema.mjs
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const weightsSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        index: true
     },
     reps: { 
         type: Number,
